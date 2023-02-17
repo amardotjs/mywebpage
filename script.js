aa8f30401ea5409cdf0454ac9dc57f94bbec8822
AmarUchiha <109662654+AmarUchiha@users.noreply.github.com>
2023-02-17 22:47:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function insta(){
+    window.open("https://www.instagram.com/amarganesh_pandian/")
+}
+function gmail(){
+    alert("amarganesh.pandian@gmail.com is my email")
+}
+function youtube(){
+    window.open("https://www.youtube.com/channel/UCeugZNs_0gyUIvytBnl0Dng")
+}
